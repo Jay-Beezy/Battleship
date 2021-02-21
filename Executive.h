0b2c15f35ef22be6f33da60c2c18b99c8bad2bb1
Chloe Thornton <chloe.kira@gmail.com>
2021-02-21 14:44:56 -0600

    added documentation for ship.h and executive.h

--- Executive.h
+++ Executive.h
@@ -12,12 +12,51 @@ class Executive
 {
 
 	public:
+		/**
+		* @pre Executive class is created
+		* @post class is created, grids are created
+		* @param none
+		* @return N/A
+		**/
 		Executive();
+		/**
+		* @pre game finishes, need to delete all arrays
+		* @post all arrays are deleted, no memory lost
+		* @param none
+		* @return N/A
+		**/
 		~Executive();
 
+		/**
+		* @pre we need the game to run
+	  * @post game is run, communicates with all other classes
+		* @param none
+		* @return N/A
+		**/
 		void run();
+
+		/**
+		* @pre need to display the playing grid of P1
+		* @post playing grid is displayed to user, has labels for each space
+		* @param none
+		* @return N/A
+		**/
 		void printPlayerOneGrid();
+
+		/**
+		* @pre need to display the playing grid of P2
+		* @post playing grid is displayed to user, has labels for each space
+		* @param none
+		* @return N/A
+		**/
 		void printPlayerTwoGrid();
+
+		/**
+		* @pre need to fill the 2d arrays for players one and two with zeros
+		* @post arrays are filled with 0s simultaneously
+		* @param none
+		* @return N/A
+		**/
 		void fillGrids();
 
 	private:
