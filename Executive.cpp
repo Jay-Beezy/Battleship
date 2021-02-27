e3420ef4cc5c070f13fda70c91eb84a478c86ef6
Steven <z654h035@ku.edu>
2021-02-27 16:09:02 -0600

    testing changes updating code

--- Executive.cpp
+++ Executive.cpp
@@ -5,30 +5,32 @@
 
 
 Executive::Executive(){ //constructor
-	playerOneGrid = new int*[10];
-	playerTwoGrid = new int*[10];
-	for(int i = 0; i < 10; i++){
-		playerOneGrid[i] = new int[10];
-		playerTwoGrid[i] = new int[10];
-	}
-	fillGrids();
+	//playerOneGrid = new int*[10];
+	//playerTwoGrid = new int*[10];
+	//for(int i = 0; i < 10; i++){
+		//playerOneGrid[i] = new int[10];
+		//playerTwoGrid[i] = new int[10];
+	//}
+	//fillGrids();
 }
 
 Executive::~Executive() //deconstructor
 {
-	for(int i = 0; i < 10; i++)
-	{
-		delete playerOneGrid[i];
-		delete playerTwoGrid[i];
-	}
-	delete[] playerOneGrid;
-	delete[] playerTwoGrid;
+	//for(int i = 0; i < 10; i++)
+	//{
+	//	delete playerOneGrid[i];
+	//	delete playerTwoGrid[i];
+	//}
+	//delete[] playerOneGrid;
+	//delete[] playerTwoGrid;
 }
 
 void Executive::run() {  //runs the program
 
-	std::string p1coords;
-	std::string p2coords;
+	char p1coords ;
+	char p2coords ;
+	int p1Number = 0;
+	int p2Number = 0;
 
 	std::cout << "+-+-+-+-+-+-+-BATTLESHIP-+-+-+-+-+-+-+\n";
 
@@ -48,7 +50,7 @@ void Executive::run() {  //runs the program
 
 	std::cout << "\nPlayer One's turn to place their ships!\n\n";
 	//Populating Player 1
-	playerOne.showWaters();
+	playerOne.showFiringBoard("One");
 	for(int i = 0; i < shipAmount; i++){
 		playerOne.anchorShips(i+1);
 	}
@@ -56,7 +58,7 @@ void Executive::run() {  //runs the program
 
 	std::cout << "\nPlayer Two's turn to place their ships!\n\n";
 	//Populating Player 2
-	playerTwo.showWaters();
+	playerTwo.showFiringBoard("Two");
 	for(int i = 0; i < shipAmount; i++){
 		playerTwo.anchorShips(i+1);
 	}
@@ -64,77 +66,90 @@ void Executive::run() {  //runs the program
 
 	//OG grid at beginning of program
 	std::cout<<"Time to Begin!\n";
-	while (isWinner()!=1 || isWinner()!=2){
+	while (1){
+			
 		std::cout<<"Player One's Firing grid:\n";
-		printPlayerOneGrid();
+		playerOne.showFiringBoard("One");
+		//printPlayerOneGrid();//These are the firing grids
 		std::cout<<"Player One's Ships:\n";
-		playerOne.showWaters();
+		playerOne.showWaters("One");// These are your ship placements
 		std::cout<<"Player One, enter coordinates of the space you'd like to fire on.\n";
+		std::cout<<"Player One, enter letter of the space you'd like to fire on: ";
 		std::cin>>p1coords;
-		playerTwo.checkGrid(p1coords);
+		std::cout<<"Player One, enter number of the space you'd like to fire on: ";
+		std::cin>>p1Number;
+		
+		playerOne.checkGrid(p1coords,p1Number, playerTwo);
+		
 		std::cout<<"Player Two's Firing grid:\n";
-		printPlayerTwoGrid();
+		playerTwo.showFiringBoard("Two");
+		//printPlayerTwoGrid();//These are the firing grids
 		std::cout<<"Player Two's Ships:\n";
-		playerTwo.showWaters();
+		playerTwo.showWaters("Two");// These are your ship placements
 		std::cout<<"Player Two, enter coordinates of the space you'd like to fire on.\n";
+		std::cout<<"Player Two, enter letter of the space you'd like to fire on: ";
 		std::cin>>p2coords;
-		playerOne.checkGrid(p2coords);
+		std::cout<<"Player Two, enter number of the space you'd like to fire on: ";
+		std::cin>>p2Number;
+		
+		playerTwo.checkGrid(p2coords,p2Number, playerOne);
+
 	}
 
 }
 
-void Executive::fillGrids(){
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			playerOneGrid[i][j] = 0;
-			playerTwoGrid[i][j] = 0;
-		}
-	}
-}
+// void Executive::fillGrids(){
+// 	for(int i = 0; i < 10; i++){
+// 		for(int j = 0; j < 10; j++){
+// 			playerOneGrid[i][j] = 0;
+// 			playerTwoGrid[i][j] = 0;
+// 		}
+// 	}
+// }
 
-void Executive::printPlayerOneGrid(){
-	std::cout << "+------------------------------------------Player One's Board-------------------------------------------+\n";
-	std::cout << "|			A	B	C	D	E	F	G	H	I	J	|\n";
-	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			if(j == 0){
-				std::cout << "|" << "\t" << i+1 << "\t" << "|" << "\t" << playerOneGrid[i][j] << "\t";
-			}
-			else if(j == 9){
-				std::cout << playerOneGrid[i][j] << "\t" << "|";
-			}
-			else{
-				std::cout << playerOneGrid[i][j] << "\t";
-			}
-		}
-		std::cout << "\n";
-	}
-	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
-}
+// void Executive::printPlayerOneGrid(){
+// 	std::cout << "+------------------------------------------Player One's Board-------------------------------------------+\n";
+// 	std::cout << "|			A	B	C	D	E	F	G	H	I	J	|\n";
+// 	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
+// 	for(int i = 0; i < 10; i++){
+// 		for(int j = 0; j < 10; j++){
+// 			if(j == 0){
+// 				std::cout << "|" << "\t" << i+1 << "\t" << "|" << "\t" << playerOneGrid[i][j] << "\t";
+// 			}
+// 			else if(j == 9){
+// 				std::cout << playerOneGrid[i][j] << "\t" << "|";
+// 			}
+// 			else{
+// 				std::cout << playerOneGrid[i][j] << "\t";
+// 			}
+// 		}
+// 		std::cout << "\n";
+// 	}
+// 	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
+// }
 
-void Executive::printPlayerTwoGrid(){
-	std::cout << "+------------------------------------------Player Two's Board-------------------------------------------+\n";
-	std::cout << "|			A	B	C	D	E	F	G	H	I	J	|\n";
-	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			if(j == 0){
-				std::cout << "|" << "\t" << i+1 << "\t" << "|" << "\t" << playerTwoGrid[i][j] << "\t";
-			}
-			else if(j == 9){
-				std::cout << playerTwoGrid[i][j] << "\t" << "|";
-			}
-			else{
-				std::cout << playerTwoGrid[i][j] << "\t";
-			}
-		}
-		std::cout << "\n";
-	}
-	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
-}
+// void Executive::printPlayerTwoGrid(){
+// 	std::cout << "+------------------------------------------Player Two's Board-------------------------------------------+\n";
+// 	std::cout << "|			A	B	C	D	E	F	G	H	I	J	|\n";
+// 	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
+// 	for(int i = 0; i < 10; i++){
+// 		for(int j = 0; j < 10; j++){
+// 			if(j == 0){
+// 				std::cout << "|" << "\t" << i+1 << "\t" << "|" << "\t" << playerTwoGrid[i][j] << "\t";
+// 			}
+// 			else if(j == 9){
+// 				std::cout << playerTwoGrid[i][j] << "\t" << "|";
+// 			}
+// 			else{
+// 				std::cout << playerTwoGrid[i][j] << "\t";
+// 			}
+// 		}
+// 		std::cout << "\n";
+// 	}
+// 	std::cout << "+---------------+---------------------------------------------------------------------------------------+\n";
+// }
 
-int Executive::isWinner(){
+int Executive::isWinner(Player& playerOne,Player& playerTwo){
 	if(playerOne.shipsRemaining() == 0) {
 		return(1);
 	} else if(playerTwo.shipsRemaining() == 0) {
