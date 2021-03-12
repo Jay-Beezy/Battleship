225840848737a0a71d45f3644e4a228f9108127e
Steven <z654h035@ku.edu>
2021-03-11 19:16:51 -0600

    commented out line that was printing array(used for debugging)

--- Ship.cpp
+++ Ship.cpp
@@ -98,7 +98,7 @@ void Ship::createArray()
 
 	 	for(int i = 0; i < 10; i++){
 		for(int j = 0; j < 10; j++){
-			std::cout << individualShipPlacements[i][j] << "     ";
+			//std::cout << individualShipPlacements[i][j] << "     ";
 		}
 		std::cout << "\n";
 	}
