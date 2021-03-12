c7892050bbd95ed2b840cde90019cf24c1dda195
Caden Kroonenberg <cskroonenberg@gmail.com>
2021-03-12 16:28:51 -0600

    Make Parent class a shell so AI and Player classes can use their own functions when called in Executive.

--- Parent.cpp
+++ Parent.cpp
@@ -1,138 +1,23 @@
 #include "Parent.h"
 #include <iostream>
 
-
-
-
 void Parent::showFiringBoard(std::string turnPlayer){
-
 }
 
-void showShipPlacement(std::string turnPlayer){
-	
+void Parent::showShipPlacement(std::string turnPlayer){
 }
 
-
-bool Parent::validatePosition(int row, int colnum, std::string direction, int shipLength)
-{
-	bool isValid = false;
-	//int temporaryRow = row;
-	if(direction=="H" || direction == "h")
-	{
-		for(int i=0;i<shipLength;i++)
-		{
-			if((colnum)<=10 && shipGrid[row-1][colnum-1]=='0'){
-				isValid = true;
-			}
-			else if(shipGrid[row-1][colnum-1]=='S')
-			{
-					std::cout <<"Ships overlapping horizontally, please try again\n";
-					return(false);
-					//isValid = false;
-					//return(isValid);
-			}
-			else
-			{
-				isValid = false;
-				std::cout << "\n\nInvalid Coordinates, try again.\n\n";
-			}
-			colnum++;
-		}
-	}
-
-	//changed int i, to int j, cuz bug fixing. - andrews
-	if(direction=="V" || direction == "v"){
-		for(int j=0;j<shipLength;j++){
-			if(row <= 10 && shipGrid[row-1][colnum-1]=='0' ){
-				isValid = true;
-			}
-			else if(row>10)
-			{
-				std::cout << "\n\nInvalid Coordinates, try again.\n\n";
-				return(false);
-			}
-			else if(shipGrid[row-1][colnum-1]=='S')
-	 		{
-	 				std::cout <<"Ships overlapping Vertically, please try again\n";
-					 return(false);
-	 				//isValid = false;
-	 		}
-			else
-			{
-				isValid = false;
-			}
-			row++;
-		}
-	}
-	return(isValid);
+void Parent::placeShips(int length){
 }
 
-
-int Parent::colToInt(char column){
-	if(column == 'A' || column == 'a'){
-		return(1);
-	}
-	else if(column == 'B' || column == 'b'){
-		return(2);
-	}
-	else if(column == 'C' || column == 'c'){
-		return(3);
-	}
-	else if(column == 'D' || column == 'd'){
-		return(4);
-	}
-	else if(column == 'E' || column == 'e'){
-		return(5);
-	}
-	else if(column == 'F' || column == 'f'){
-		return(6);
-	}
-	else if(column == 'G' || column == 'g'){
-		return(7);
-	}
-	else if(column == 'H' || column == 'h'){
-		return(8);
-	}
-	else if(column == 'I' || column == 'i'){
-		return(9);
-	}
-	else if(column == 'J' || column == 'j'){
-		return(10);
-	}
-	return(0);
-}
-
-char Parent::getShotGrid(char letter, int input)
-{
-	int colnum = colToInt(letter);
-	//std:: cout << shotGrid[input-1][colnum-1]<<"\n";
-	return shotGrid[input-1][colnum-1];
+void Parent::takeShot(Parent* opp){
 }
 
 void Parent::shipHit(Ship& ship)
 {
-	ship.shipMinusHealth();
-	if(ship.getHealth() !=ship.getLength())
-	{
-		ship.checkIfSunk();
-	}
-	
-}
-
-bool Parent::validateCol(char column){
-	return (column >='a' && column <='j') || (column >='A' && column <='J');
-}
-
-std::string Parent::intToString(int num){
-	std::string temp = std::to_string(num);
-	return(temp);
-}
-
-bool Parent::validateRow(int row){
-	return (row >= 1 && row <= 10);
 }
 
 const int Parent::shipsRemaining()
 {
-	return m_numberOfShips;
+	return 0;
 }
--- Parent.h
+++ Parent.h
@@ -11,64 +11,8 @@ class Parent
 {
     public:
 
-    	void showFiringBoard(std::string turnPlayer); // shows top board where you fire your shots
-		void showShipPlacement(std::string turnPlayer); // shows bottom board where youre ships are placed
-
-		/**
-		* @pre A column value must be checked to see if it is valid
-		* @post returns true if the column is a valid proposal
-		* @param char column, the column value to check
-		* @return bool, is the column valid
-		**/
-		bool validateCol(char column);
-
-		/**
-		* @pre A row value must be checked to see if it is valid
-		* @post returns true if the row is a valid proposal
-		* @param int row, the row value to check
-		* @return bool, is the row valid
-		**/
-		bool validateRow(int row);
-
-		/**
-		* @pre a ship has been created, and it is not known if it's position is valid
-		* @post returns rue if the position is valid
-		* @param int row, the row the 'head' of the ship is located on
-		* @param int colnum, the column the 'head' of the ship is located on
-		* @param std::string direction, the direction the ship is heading, H or V
-		* @param int size, the length of the ship
-		* @return bool, is the position is valid
-		**/
-		bool validatePosition(int row, int colnum, std::string direction, int size);
-
-		/**
-		* @pre we need to turn an int to a string
-		* @post we turned an int into a string
-		* @param int num, number we're turning into a string
-		* @return std::string, the string
-		**/
-		std::string intToString(int num);
-
-		/**		
-		* @pre the player has inputted coordinates to fire upon, we need to process them
-		* @post checks if the ship has been hit, if not hits area with power shot
-		* @param std::string shipCoords, the location of the ship
-		* @return none
-		**/
-		void checkPower(char letterInput, int numberInput,Parent& otherPlayer);
-
-		/**
-		* @pre all ships have been constructed
-		* @post checks how many ships have not been destroed
-		* @return number of ships not destroyed
-		**/
-		const int shipsRemaining();
-
-		void shipHit(Ship& ship);
-		
-		char getShotGrid(char letter, int number);
-
-        int colToInt(char column);
+    	virtual void showFiringBoard(std::string turnPlayer); // shows top board where you fire your shots
+		virtual void showShipPlacement(std::string turnPlayer); // shows bottom board where youre ships are placed
 
 		/**
 		* @pre The ship must be created and anchored to a spot on player grid
@@ -76,22 +20,31 @@ class Parent
 		* @param int length, how long that ships is
 		* @return void
 		**/
-
-		bool charge; //variable for if player has a power shot charge	
-
+		virtual void placeShips(int length);
 
 		/**
 		* @pre the player selects coordinates to fire upon
 		* @post area will be hit (or not) and opponent ship grid will be adjusted accordingly
 		* @param opp the opponent to be fired upon
 		**/
-		void takeShot(Parent& opp);
+		virtual void takeShot(Parent* opp);
+
+		/**
+		* @pre all ships have been constructed
+		* @post checks how many ships have not been destroed
+		* @return number of ships not destroyed
+		**/
+		virtual const int shipsRemaining();
+
+		virtual void shipHit(Ship& ship);
+
     //protected:
         int m_ships;
 		int m_numberOfShips;
 		char** shipGrid;
 		char** shotGrid;
 		Ship** shipArray;//max number of ships, allows us to have a location for each ship in memory
+		bool charge;
 
 };
 #endif
