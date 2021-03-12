c74ef3e91d8c3e56867cb1b269da1b518789bd1c
Steven <z654h035@ku.edu>
2021-03-11 19:17:41 -0600

    fixed small bug in constructor

--- AI.cpp
+++ AI.cpp
@@ -14,7 +14,7 @@ AI::AI(int difficulty, int ships){
         shipGrid[i] = new char[10];
     }
     for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; i++){
+        for(int j = 0; j < 10; j++){
             shotGrid[i][j] = '0';
             shipGrid[i][j] = '0';
         }
